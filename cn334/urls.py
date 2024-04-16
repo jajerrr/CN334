7e72c6fb83c859269d8351a247bd25f93bfa7951
jajerrr <107903870+jajerrr@users.noreply.github.com>
2024-04-16 19:50:33 +0700

    up

--- cn334/urls.py
+++ cn334/urls.py
@@ -1,25 +1,6 @@
-"""
-URL configuration for cn334 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-
-
 from ecommerce import views as ecom_views
-
 
 urlpatterns =[
 
@@ -30,6 +11,10 @@ urlpatterns =[
     path("ecommerce/Contactpage", ecom_views.Contactpage),
     path("ecommerce/Categorypage", ecom_views.Categorypage),
     path("ecommerce/Checkoutpage", ecom_views.Checkoutpage),
-    path("ecommerce/Productpage", ecom_views.Productpage)
+    path("ecommerce/Productpage", ecom_views.Productpage),
+    path("w09/request", ecom_views.basic_request),
+    path("w09/tokenize", ecom_views.tokenize),
+    path("w09/sentimental", ecom_views.sentimental),
+    path("w09/text2speech", ecom_views.text2speech)
     
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,8 @@
+import requests
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def ecommerce_index_view(request):
@@ -7,9 +10,9 @@ def ecommerce_index_view(request):
 
     return HttpResponse('Welcome to 6310742140 Achiraya Komatsu views!')
 
-def item_view(request, item_id):   
+def item_view(request, item_id):
     context_data = {
-        "item_id": item_id   
+        "item_id": item_id
     }
     return render(request, 'index.html',context= context_data)
 def Homepage(request):
@@ -26,3 +29,61 @@ def Checkoutpage(request):
 
 def Contactpage(request):
     return render(request, 'Contactpage.html')
+
+@csrf_exempt
+def basic_request(request):
+    if request.method == "GET":
+        return JsonResponse({"status":"GET Pass"}, safe=False)
+    if request.method == "POST":
+        return JsonResponse({"status":"POST Pass"}, safe=False)
+    
+@csrf_exempt
+def tokenize(request):
+    if request.method == "POST":
+        try:
+            sentence = request.POST["text"]
+        except:
+            return JsonResponse({"error":"Input not found"}, safe=False, status=500)
+        url = "https://api.aiforthai.in.th/tlexplus"
+        data = {"text":sentence}
+        headers = {
+            'Apikey': "d9Lqve8DbtLqLAe9C8mHZgq3cSbpM9BD"
+            }
+        response = requests.post(url, data=data, headers=headers)
+        response_json = response.json()
+        return JsonResponse ({"student":"student_id", "tokenize": response_json} , safe=False)
+    return JsonResponse ({"error":"Method not allowed!"} , safe=False , status = 403)
+
+@csrf_exempt
+def sentimental(request):
+    if request.method == "POST":
+        try:
+            sentence = request.POST["text"]
+        except:
+            return JsonResponse({"error":"Input not found"}, safe=False, status=500)
+        url = "https://api.aiforthai.in.th/ssense"
+        data = {"text":sentence}
+        headers = {
+            'Apikey': "d9Lqve8DbtLqLAe9C8mHZgq3cSbpM9BD"
+            }
+        response = requests.post(url, data=data, headers=headers)
+        response_json = response.json()
+        return JsonResponse ({"student":"student_id", "sentimental": response_json} , safe=False)
+    return JsonResponse ({"error":"Method not allowed!"} , safe=False , status = 403)
+
+@csrf_exempt
+def text2speech(request):
+    if request.method == "POST":
+        try:
+            sentence = request.POST["text"]
+        except:
+            return JsonResponse({"error":"Input not found"}, safe=False, status=500)
+        url = "https://api.aiforthai.in.th/vaja9/synth_audiovisual"
+        data = {"text":sentence}
+        headers = {
+            'Apikey': "d9Lqve8DbtLqLAe9C8mHZgq3cSbpM9BD"
+            }
+        response = requests.post(url, data=data, headers=headers)
+        response_json = response.json()
+        return JsonResponse ({ "student":"student_id" , "output":"https://api.aiforthai.in.th/vaja9/synth_audiovisual"} , safe=False)
+    #return JsonResponse ({"error":"Method not allowed!"} , safe=False , status = 403)
